982a19c0b45d0cd6303ae1e5beb52798f382aea3
Alejandro Echeverria <alejandro@echev.com>
2018-04-10 16:31:06 -0300

    refactor(): refactor wrapper and module

--- lib/bandiera_client.rb
+++ lib/bandiera_client.rb
@@ -1,35 +1,17 @@
 require 'bandiera/client'
 
 module Bandiera
-  @@host = 'localhost'
+  class << self
+    attr_accessor :host
+    attr_accessor :port
 
-  @@port = '5000'
+    def configure
+      yield self
+      @client = Client.new("http://#{@host}:#{@port}")
+    end
 
-  @@client = Client.new("http://#{@@host}:#{@@port}")
-
-  def self.configure
-    yield self
-    @@client = Client.new("http://#{@@host}:#{@@port}")
+    def on?(_feature, _group = 'All', _params = {}, _http_options = {})
+      @client.enabled?(_group, _feature, _params, _http_options)
+    end
   end
-
-  def self.host
-    @@host
-  end
-
-  def self.host=(_host)
-    @@host = _host
-  end
-
-  def self.port
-    @@port
-  end
-
-  def self.port=(_port)
-    @@port = _port
-  end
-
-  def self.on?(_feature, _group = "All", _params = {}, _http_options = {})
-    @@client.enabled?(_group, _feature, _params, _http_options)
-  end
-
 end
