bf3826ddaf3cb46789abeccf606d7974b26d8847
Alejandro Echeverria <alejandro@echev.com>
2018-04-11 10:41:23 -0300

    feat(): allows configuration of default group name

--- lib/bandiera_client.rb
+++ lib/bandiera_client.rb
@@ -4,13 +4,14 @@ module Bandiera
   class << self
     attr_accessor :host
     attr_accessor :port
+    attr_accessor :default_group
 
     def configure
       yield self
       @client = Client.new("http://#{@host}:#{@port}")
     end
 
-    def on?(_feature, _group = 'All', _params = {}, _http_options = {})
+    def on?(_feature, _group = @default_group || 'All', _params = {}, _http_options = {})
       @client.enabled?(_group, _feature, _params, _http_options)
     end
   end
